4a773f822db25c8b09379fdffec1f20edadf2148
Marko Vukobrat <marko.vukobrat@buynomics.com>
2023-03-30 23:42:53 +0200

    add comments to js file

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Initialize fullpage.js when the DOM content is loaded
 document.addEventListener('DOMContentLoaded', function() {
   new fullpage('#fullpage', {
       sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
@@ -8,13 +9,14 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
+// Get the gallery element and the images to display
 var gallery = document.querySelector('.image-list');
 var images = ['images/house3.jpg', 'images/house2.jpg', 'images/house4.jpg'];
 var currentIndex = 0;
-
+// Get all image links inside image-container elements and load the actual images on page load
 var images = document.querySelectorAll(".image-container a");
 
-
+// Loop through all the image links and set their source attribute to the value of their data-src attribute
 function loadImages() { 
   for (var i = 0; i < images.length; i++) {
     var image = images[i];
@@ -22,7 +24,6 @@ function loadImages() {
   }
 }
 
-// be sure that loadImages method is called 
-// when the page is loaded
+// Ensure that the loadImages function is called when the page is loaded
 window.addEventListener("load", loadImages);
 
