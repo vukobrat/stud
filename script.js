032793dba17958e687bdb595da90ff2dd7268e44
Marko Vukobrat <marko.vukobrat@buynomics.com>
2023-03-30 18:07:13 +0200

    translate to latin

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
       sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
       anchors: ['home', 'about', 'rent', 'gallery', 'contact'],
       navigation: true,
-      navigationTooltips: ['Home', 'About Us', 'Rent', 'Gallery', 'Contact']
+      navigationTooltips: ['Почетна', 'О нама', 'Смештај', 'Галерија', 'Контакт']
   });
 });
 
@@ -12,8 +12,20 @@ var images = ['images/house1.jpg', 'images/house2.jpg', 'images/house3.jpg'];
 var currentIndex = 0;
 
 new fullpage('#fullpage', {
-  scrollingSpeed: 100
+  scrollingSpeed: 70
 });
 
 var images = document.querySelectorAll(".image-container img");
+
+
+function loadImages() { 
+  for (var i = 0; i < images.length; i++) {
+    var image = images[i];
+    image.src = image.dataset.src;
+  }
+}
+
+// be sure that loadImages method is called 
+// when the page is loaded
+window.addEventListener("load", loadImages);
 
